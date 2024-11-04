18c4f8ae552e4e4c630b2607423fb2a832dd3b7b
Benjamin Lin <benlin46@unc.edu>
2024-11-04 00:55:43 -0500

    path changes

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 // main.js
 
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.170.0/build/three.module.js';
-
-
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import { EXRLoader } from 'three/examples/jsm/loaders/EXRLoader';
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.170.0/examples/jsm/controls/OrbitControls.js';
+import { EXRLoader } from 'https://cdn.jsdelivr.net/npm/three@0.170.0/examples/jsm/loaders/EXRLoader.js';
 
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("button").addEventListener("click", () => {
